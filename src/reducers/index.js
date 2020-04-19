06c8a0de79962e595e33086f46a29522237ceb2d
Karen Nguyen <knguyen4184@yahoo.com>
2020-04-19 15:11:20 -0600

    Error message

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { showDetail } from './showDetail';
 import { makeup } from './makeup';
 import { favorite } from './favorite';
+import { error } from './error';
 import { combineReducers } from 'redux';
 
 export const RootReducer = combineReducers({
   showDetail,
   makeup,
   favorite,
+  error
 });
